e6672fcfb71cd9c65a3438b36fb867532057c22d
LirionOne <79844169+LirionOne@users.noreply.github.com>
2021-07-31 03:14:23 +1000

    Add files via upload
    fixed bugs

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import sys
 
-from PyQt5.QtWidgets import QMainWindow, QDialog, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication
 import requests
-import json
-from main_window import Ui_MainWindow
-from date_choose import Ui_Dialog
 
-from datetime import date
+from main_window import Ui_MainWindow
 
 KEY = "ca9b69f89f27cc894e665e13a06b26c5"
 URL = "https://api.openweathermap.org/data/2.5/weather/"
@@ -16,17 +13,16 @@ class Weather_app(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        # city_name = self.lineEdit.text()
-        city_name = input()
-        self.weather_button.clicked.connect(self.get_city_weather(city_name))
 
-    # class Get_Weather():
+        self.weather_button.clicked.connect(self.get_city_weather)
+
     def is_response_good(self, response):
         if 200 <= response.status_code < 300:
             return True
 
-    def get_city_weather(self, city_name):
-        lang = "ru"
+    def get_city_weather(self):
+        city_name = self.lineEdit.text()
+        lang = "en"
         units = "metric"
         query = {
             "q": city_name,
@@ -44,14 +40,18 @@ class Weather_app(QMainWindow, Ui_MainWindow):
             feel_like = result['main']['feels_like']
             pressure = result['main']['pressure']
             humidity = result['main']['humidity']
+            wind_speed = result['wind']['speed']
+            visibility = result['visibility']
 
             out = ""
             out += str(description) + '\n'
-            out += str(temperature) + '\n'
-            out += str(feel_like) + '\n'
-            out += str(pressure) + '\n'
-            out += str(humidity) + '\n'
-            # out = description + '\n' + temperature + '\n' + feel_like + '\n' + pressure + '\n' + humidity
+            out += 'Temperature: ' + str(temperature) + '°C' + '\n'
+            out += 'Feels like: ' + str(feel_like) + '°C' + '\n'
+            out += 'Pressure: ' + str(pressure) + ' hPa' + '\n'
+            out += 'Humidity: ' + str(humidity) + '%' + '\n'
+            out += 'Wind speed: ' + str(wind_speed) + ' m/s' + '\n'
+            out += 'Visibility: ' + str(visibility) + ' m' + '\n'
+
             self.textBrowser.setText(out)
         else:
             self.textBrowser.setText('Something went wrong')
